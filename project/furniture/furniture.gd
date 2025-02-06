d3dd1403bf14dccc7f9413281636193a5111fc09
Camden Hovell <camden.hovell@bsu.edu>
2025-02-06 11:54:16 -0500

    Add more furniture types

--- project/furniture/furniture.gd
+++ project/furniture/furniture.gd
@@ -23,5 +23,6 @@ func _ready() -> void:
 	collision_shape.position.y = collision_shape.shape.size.y / 2.0
 	
 	price_label.position.x = -1 * collision_shape.shape.size.x / 2.0
-	#price_label.position.z = collision_shape.shape.size.z / 2.0
+	#price_label.position.z = (-1 * collision_shape.shape.size.z) + (collision_shape.shape.size.z / 2.0)
+	price_label.position.z = -3
 	price_label.position.y = collision_shape.shape.size.y / 2.0
--- project/furniture/types/furniture_type.gd
+++ project/furniture/types/furniture_type.gd
@@ -2,7 +2,7 @@ class_name FurnitureType
 extends Resource
 
 @export var name : String
-@export_enum("Bathroom", "Bed", "Table", "Chair", "Storage", "Decoration", "Door") var category : String
+@export_enum("Bathroom", "Bed", "Table", "Chair", "Storage", "Lighting", "Decoration", "Door") var category : String
 @export var price : int
 @export var model_path : String
 @export var tiles_length : int
