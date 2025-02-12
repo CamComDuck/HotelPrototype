a89e2324bbfa4e8cf74a45152fe1b1263c5724b4
Brayden Fairchild <153120388+brfairchild@users.noreply.github.com>
2025-02-12 17:37:05 -0500

    Add purchasing menu and money logic.
    Adding a basic purchasing menu that will display the items that can be purchased, is very rough right now but can be fined tuned into different styles/rooms.

--- project/character/character.gd
+++ project/character/character.gd
@@ -9,54 +9,71 @@ var interacting_furniture : Furniture
 var carrying_furniture : FurnitureType
 var last_direction : Vector3
 var allow_movement := true
+var money: int = 1000  # Starting money
 
 @onready var figurine := %Figurine as Figurine
 @onready var carrying_marker := %CarryingMarker as Marker3D
 @onready var shape_cast := %ShapeCast as ShapeCast3D
 
-
 func _physics_process(delta: float) -> void:
 	if allow_movement:
 		handle_movement(delta)
-	
+
 	if shape_cast.is_colliding():
 		if shape_cast.get_collider(0) is Furniture:
 			interacting_furniture = shape_cast.get_collider(0)
 	else:
 		interacting_furniture = null
-	
+
 	if Input.is_action_just_pressed("interact_primary"):
 		if interacting_furniture != null and carrying_furniture == null: 
-			figurine.play_animation("pick-up")
-			allow_movement = false
-			await figurine.animation_finished
-			
-			# Pick up furniture
-			allow_movement = true
-			var hand_model := interacting_furniture.model.duplicate()
-			hand_model.scale = Vector3(0.5, 0.5, 0.5)
-			carrying_marker.add_child(hand_model)
-			figurine.play_animation("holding-both")
-			
-			carrying_furniture = interacting_furniture.furniture_type
-			interacting_furniture.queue_free()
-			interacting_furniture = null
-		
+			# Getting price from .tres
+			var furniture_price = interacting_furniture.furniture_type.price
+
+			 # Deduct money if affordable
+			if spend_money(furniture_price):
+				figurine.play_animation("pick-up")
+				allow_movement = false
+				await figurine.animation_finished
+
+				# Pick up furniture
+				allow_movement = true
+				var hand_model := interacting_furniture.model.duplicate()
+				hand_model.scale = Vector3(0.5, 0.5, 0.5)
+				carrying_marker.add_child(hand_model)
+				figurine.play_animation("holding-both")
+
+				carrying_furniture = interacting_furniture.furniture_type
+				interacting_furniture.queue_free()
+				interacting_furniture = null
+			else:
+				print("Not enough money to purchase this furniture!")
+
 		elif carrying_marker.get_child_count() > 0:
 			figurine.play_animation("pick-up")
 			allow_movement = false
 			await figurine.animation_finished
-			
+
 			# Put furniture down
+			carrying_furniture.price = 0
 			furniture_placed.emit(carrying_furniture, global_position)
 			carrying_marker.get_child(0).queue_free()
 			allow_movement = true
 			carrying_furniture = null
-		
+
 	elif Input.is_action_just_pressed("interact_secondary"):
 		if interacting_furniture != null and carrying_furniture == null: 
 			interacting_furniture.rotation_degrees.y += 90
-	
+
+func spend_money(amount: int) -> bool:
+	""" Deducts money if the player has enough. Returns true if successful. """
+	if money >= amount:
+		money -= amount
+		print("Purchased! Remaining money:", money)
+		return true
+	else:
+		print("Not enough money!")
+		return false
 
 func handle_movement(delta : float) -> void:
 	# Add the gravity.
@@ -64,31 +81,28 @@ func handle_movement(delta : float) -> void:
 		velocity += get_gravity() * delta
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	
+
 	if direction != Vector3.ZERO:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 		if carrying_marker.get_child_count() == 0:
 			figurine.play_animation("walk")
-		
+
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 		if carrying_marker.get_child_count() == 0:
 			figurine.play_animation("idle")
-		
+
 	if direction != Vector3.ZERO:
 		last_direction = direction
-	
+
 	shape_cast.target_position.x = last_direction.x * 5
 	shape_cast.target_position.z = last_direction.z * 5
-	
-	
-	# If you have a better way to make him face the right direction,
-	# PLEASE refactor this lol
+
+	# Makes the character face the right direction
 	if last_direction.x > 0 and last_direction.z > 0:
 		figurine.rotation_degrees.y = 45
 	elif last_direction.x < 0 and last_direction.z < 0:
--- project/room_builder/room_builder.gd
+++ project/room_builder/room_builder.gd
@@ -3,7 +3,16 @@ extends Node3D
 
 var furniture := preload("res://furniture/furniture.tscn") as PackedScene
 
-
+func _ready() -> void:
+	# I know this is quick and dirty but it works for now.
+		$Furniture.visible = false
+		$Furniture7.visible = false
+		$Furniture2.visible = false
+		$Furniture3.visible = false
+		$Furniture6.visible = false
+		$Furniture4.visible = false
+		$Furniture5.visible = false
+		
 func _on_character_furniture_placed(furniture_placed: FurnitureType, position_placed: Vector3) -> void:
 	var new_furniture := furniture.instantiate() as Furniture
 	new_furniture.furniture_type = furniture_placed
@@ -11,3 +20,15 @@ func _on_character_furniture_placed(furniture_placed: FurnitureType, position_pl
 	add_child(new_furniture)
 	new_furniture.global_position.x = position_placed.x + 5
 	new_furniture.global_position.z = position_placed.z + 5
+
+
+func _on_purchase_square_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		# I know this is quick and dirty but it works for now.
+		$Furniture.visible = true
+		$Furniture7.visible = true
+		$Furniture2.visible = true
+		$Furniture3.visible = true
+		$Furniture6.visible = true
+		$Furniture4.visible = true
+		$Furniture5.visible = true
